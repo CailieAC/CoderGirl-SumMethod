c3c741a24961bfdc42adc39e8ae571aebf30315d
Carlile Upfront <carlileupfront@users.noreply.github.com>
2019-03-02 15:06:39 -0600

    commit1

--- SumMethod/Program.cs
+++ SumMethod/Program.cs
@@ -6,10 +6,20 @@ namespace SumMethod
     {
         public static void Main()
         {
+            Console.Write("Enter a number: ");
+            int numberInput = int.Parse(Console.ReadLine());
+            int sum = Sum(numberInput);
+            Console.WriteLine(sum);
             Console.ReadLine();
         }
 
         // TODO: Create a method that returns the sum of all integer numbers up to and including given value. 
         // Use the equation (n * (n + 1)) / 2.
+        public static int Sum(int numberInput)
+        {
+            int sum = 0;
+            sum = (numberInput * (numberInput + 1)) / 2;
+            return sum;
+        }
     }
 }
